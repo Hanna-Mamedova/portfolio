e21c199ce52ba10c4cd9270214e1cc73cdd7bf48
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-02-18 23:27:06 +0100

    feat: generate about component

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
