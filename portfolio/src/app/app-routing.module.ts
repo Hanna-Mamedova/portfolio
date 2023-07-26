d864a513e63d3f56b74aadbbbd3850b16908740e
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-07-26 20:58:23 +0200

    feat: add route to main

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/main/about/about.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component:  MainComponent },
+  // { path: 'second', component: SecondComponent },
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: '**', redirectTo: '/main' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
