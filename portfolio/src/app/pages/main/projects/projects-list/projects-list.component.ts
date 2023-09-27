6046fd93a6c64fa16d8e9f7a680d517c5de9291e
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-09-27 22:11:50 +0200

    feat: add adaptivity

--- portfolio/src/app/pages/main/projects/projects-list/projects-list.component.ts
+++ portfolio/src/app/pages/main/projects/projects-list/projects-list.component.ts
@@ -8,29 +8,34 @@ import { projects } from 'src/app/shared/projects.data';
 @Component({
   selector: 'app-projects-list',
   templateUrl: './projects-list.component.html',
-  styleUrls: ['./projects-list.component.scss']
+  styleUrls: ['./projects-list.component.scss'],
 })
 export class ProjectsListComponent implements OnInit {
-  column = 8
-  currentProjects = projects
-  backgroundColor = Color.secondary
-  subscription: Subscription
+  column = 8;
+  currentProjects = projects;
+  backgroundColor = Color.secondary;
+  subscription: Subscription;
 
-  constructor(private router: Router, private passFiltersService: PassChosenFiltersService) {}
+  constructor(
+    private router: Router,
+    private passFiltersService: PassChosenFiltersService,
+  ) {}
 
   ngOnInit() {
-    this.subscription = this.passFiltersService.currentFilters.subscribe(
-      filters => {
-        const filteredProjects = projects.filter(project => {
-          return filters?.some(filter => project.technologies['core'].includes(filter));
-        })
-        this.currentProjects = filteredProjects.length ? filteredProjects : projects;
-      }
-    )
+    this.subscription = this.passFiltersService.currentFilters.subscribe((filters) => {
+      const filteredProjects = projects.filter((project) => {
+        return filters?.some((filter) => project.technologies['core'].includes(filter));
+      });
+      this.currentProjects = filteredProjects.length ? filteredProjects : projects;
+    });
   }
 
   getColspan(i: number): number {
-    return Math.floor(i / 2) % 2 === 0 ? (i % 2 === 0 ? 5 : 3) : (i % 2 === 0 ? 3 : 5);
+    if (window.innerWidth < 800) {
+      return 8;
+    }
+
+    return Math.floor(i / 2) % 2 === 0 ? (i % 2 === 0 ? 5 : 3) : i % 2 === 0 ? 3 : 5;
   }
 
   showProjectInfo(id: number): void {
